768323ffb2f88dd36399cf42dfdf2571c6651fc0
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-08 11:45:52 +0500

    Update 6_11.php

--- tasks/Shangin/6_Shangin/6_11.php
+++ tasks/Shangin/6_Shangin/6_11.php
@@ -1,67 +1,63 @@
 <?php
-	function func($arr) {
-		foreach ($arr as $elem) {
-			if (is_array($elem)) {
-				func($elem);
-			} else {
-				echo $elem;
-			}
-		}
+    function func($arr) {
+        foreach ($arr as $elem) {
+	    if (is_array($elem)) {
+		func($elem);
+	    } else {
+		echo $elem;
+	    }
 	}
+    }
 	
 	func([1, 2, 3, [4, 5, [6, 7]], [8, [9, 10]]]);
 ?>
 <br>
 <?php
-	function func2($arr) {
-		$sum = 0;
-		
-		foreach ($arr as $elem) {
-			if (is_array($elem)) {
-				$sum += func2($elem);
-			} else {
-				$sum += $elem;
-			}
-		}
-		
-		return $sum;
+    function func2($arr) {
+	$sum = 0;
+	foreach ($arr as $elem) {
+	    if (is_array($elem)) {
+                $sum += func2($elem);
+	    } else {
+	        $sum += $elem;
+	    }
 	}
-	
-	var_dump(func2([1, 2, 3, [4, 5, [6, 7]], [8, [9, 10]]]));
+	return $sum;
+    }
+    var_dump(func2([1, 2, 3, [4, 5, [6, 7]], [8, [9, 10]]]));
 ?>
 <br>
 <?php
-	function func3($arr) {
-		$sum = '';
+    function func3($arr) {
+        $sum = '';
 		
-		foreach ($arr as $elem) {
-			if (is_array($elem)) {
-				$sum .= func3($elem);
-			} else {
-				$sum .= $elem;
-			}
-		}
-		
-		return $sum;
+	foreach ($arr as $elem) {
+	    if (is_array($elem)) {
+	        $sum .= func3($elem);
+	    } else {
+	        $sum .= $elem;
+	    }
 	}
-	
-	var_dump(func3(['a', ['b', 'c', 'd'], ['e', 'f', ['g', ['j', 'k']]]]));
+		
+	return $sum;
+    }
+    var_dump(func3(['a', ['b', 'c', 'd'], ['e', 'f', ['g', ['j', 'k']]]]));
 ?>
 <br>
 <?php
-	function func4($arr) {
-		$length = count($arr);
+    function func4($arr) {
+        $length = count($arr);
 		
-		for ($i = 0; $i < $length; $i++) {
-			if (is_array($arr[$i])) {
-				$arr[$i] = func4($arr[$i]);
-			} else {
-				$arr[$i] = $arr[$i] * $arr[$i];
-			}
-		}
+        for ($i = 0; $i < $length; $i++) {
+	    if (is_array($arr[$i])) {
+	        $arr[$i] = func4($arr[$i]);
+	    } else {
+	        $arr[$i] = $arr[$i] * $arr[$i];
+	    }
+        }
 		
-		return $arr;
-	}
+        return $arr;
+    }
 	
-	var_dump(func4([1, [2, 7, 8], [3, 4], [5, [6, 7]]]));
+    var_dump(func4([1, [2, 7, 8], [3, 4], [5, [6, 7]]]));
 ?>
