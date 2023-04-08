628c393329dd83d7e7958fbcbf23d6ef821385c1
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-08 12:17:08 +0500

    Update 8_2.php

--- tasks/Shangin/8_Shangin/8_2.php
+++ tasks/Shangin/8_Shangin/8_2.php
@@ -1,8 +1,8 @@
 <form action="8_2.php" method="GET">
-	<input name="num1" placeholder="num1">
+    <input name="num1" placeholder="num1">
     <input name="num2" placeholder="num2">
-	<input name="num3" placeholder="num3">
-	<input type="submit">
+    <input name="num3" placeholder="num3">
+    <input type="submit">
 </form>
 <?php
     var_dump ($_GET['num1'] + $_GET['num2'] + $_GET['num3']);
@@ -10,9 +10,9 @@
 <br>
 
 <form action="8_2.php" method="POST">
-	<input name="имя" placeholder="имя">
+    <input name="имя" placeholder="имя">
     <input name="возраст" placeholder="возраст">
-	<input type="submit">
+    <input type="submit">
 </form>
 <?php
     var_dump ($_POST['имя'] . $_POST['возраст']);
@@ -20,8 +20,8 @@
 <br>
 
 <form action="8_2.php" method="POST">
-	<input type="password" name="pass" placeholder="пароль">
-	<input type="submit">
+    <input type="password" name="pass" placeholder="пароль">
+    <input type="submit">
 </form>
 <?php
     if ($_POST["pass"] == 12345){
