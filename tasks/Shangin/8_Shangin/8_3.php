541f8a8557771ea4ea7b962df19e5d035a5fd5f0
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-08 12:17:33 +0500

    Update 8_3.php

--- tasks/Shangin/8_Shangin/8_3.php
+++ tasks/Shangin/8_Shangin/8_3.php
@@ -1,12 +1,12 @@
 <form action="" method="GET">
-	<input name="Фамилия" placeholder="Фамилия">
+    <input name="Фамилия" placeholder="Фамилия">
     <input name="Имя" placeholder="Имя">
     <input name="Отчество" placeholder="Отчество">
-	<input type="submit">
+    <input type="submit">
 </form>
 
 <?php
-	if (!empty($_GET)) {
-		echo $_GET['Фамилия'] . $_GET['Имя'] . $_GET['Отчество'];
-	}
+    if (!empty($_GET)) {
+        echo $_GET['Фамилия'] . $_GET['Имя'] . $_GET['Отчество'];
+    }
 ?>
