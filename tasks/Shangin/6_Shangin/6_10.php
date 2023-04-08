8fba8b9c5f4214c811ee17c9dfd9905ee7215332
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-08 11:38:11 +0500

    Update 6_10.php

--- tasks/Shangin/6_Shangin/6_10.php
+++ tasks/Shangin/6_Shangin/6_10.php
@@ -1,21 +1,21 @@
 <?php
-	function func($arr) {
-		var_dump($arr);
-	}
+    function func($arr) {
+	var_dump($arr);
+    }
 	
-	func(['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5]);
+    func(['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5]);
 ?>
 <br>
 <?php
-	function getSum($arr) {
-		$sum = array_shift($arr);
+    function getSum($arr) {
+	$sum = array_shift($arr);
 		
-		if (count($arr) !== 0) {
-			$sum += getSum($arr);
-		}
-		
-		return $sum;
+	if (count($arr) !== 0) {
+	    $sum += getSum($arr);
 	}
+		
+	return $sum;
+    }
 	
-	var_dump(getSum(['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5]));
+    var_dump(getSum(['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5]));
 ?>
