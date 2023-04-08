e59d77d79d35e82d66ed5a99f6bcbae263b4aa6b
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-08 12:12:03 +0500

    Update 8_11.php

--- tasks/Shangin/8_Shangin/8_11.php
+++ tasks/Shangin/8_Shangin/8_11.php
@@ -1,16 +1,16 @@
 <?php
-echo $_GET['num'].'<br>';
+    echo $_GET['num'].'<br>';
 
-echo '<a href="?num=1">par 1</a>'.'<br>';
-echo '<a href="?num=2">par 2</a>'.'<br>';
-echo '<a href="?num=3">par3 </a>'.'<br>';
+    echo '<a href="?num=1">par 1</a>'.'<br>';
+    echo '<a href="?num=2">par 2</a>'.'<br>';
+    echo '<a href="?num=3">par3 </a>'.'<br>';
 ?>
 <br>
 <?php
-echo $_GET['num'].'<br>';
+    echo $_GET['num'].'<br>';
 
-for($i = 1; $i <= 10; $i++){
-echo "<a href=\"?num=$i\">link $i</a><br>";
+    for($i = 1; $i <= 10; $i++){
+    echo "<a href=\"?num=$i\">link $i</a><br>";
 }
 ?>
 <br>
@@ -18,7 +18,7 @@ echo "<a href=\"?num=$i\">link $i</a><br>";
     $arr = ['a', 'b', 'c', 'd', 'e'];
 
     if(isset($_GET['num']) && isset($arr[$_GET['num']])){
-    echo $arr[$_GET['num']].'<br>';
+        echo $arr[$_GET['num']].'<br>';
     }
     echo "<a href=\"?num=0\">link 0</a><br>";
     echo "<a href=\"?num=1\">link 1</a><br>";
