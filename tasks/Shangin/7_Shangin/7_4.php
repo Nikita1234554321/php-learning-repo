f18bf5254bbd884c9c8e840b00ca1c522bac5961
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-08 12:05:02 +0500

    Update 7_4.php

--- tasks/Shangin/7_Shangin/7_4.php
+++ tasks/Shangin/7_Shangin/7_4.php
@@ -1,11 +1,11 @@
 <?php
-	for ($i = 1; $i <= 5; $i++) {
-		echo "<ul><li>$i</li></ul>";
-	}
+    for ($i = 1; $i <= 5; $i++) {
+        echo "<ul><li>$i</li></ul>";
+    }
 ?>
 <br>
 <?php
-	$arr = ['text1', 'text2', 'text3'];
+    $arr = ['text1', 'text2', 'text3'];
     foreach ($arr as $elem){
         echo "<select><option>$elem</option></select>";
     }
