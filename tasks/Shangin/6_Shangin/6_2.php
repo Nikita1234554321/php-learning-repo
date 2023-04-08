5e6fbce0e0dc79247e5ff425badedb0f4cfac291
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-08 11:54:40 +0500

    Update 6_2.php

--- tasks/Shangin/6_Shangin/6_2.php
+++ tasks/Shangin/6_Shangin/6_2.php
@@ -1,33 +1,33 @@
 <?php
-	function func($num) {
-		echo $num * $num * $num;
-	}
-	func(2); 
+    function func($num) {
+	echo $num * $num * $num;
+    }
+    func(2); 
 ?>
 <br>
 <?php
-	function func1($num) {
-		if ($num > 0){
-            echo '+++';
-        }   else '---';
-	}
-	func1(2); 
+    function func1($num) {
+	if ($num > 0){
+	    echo '+++';
+	}   else '---';
+    }
+    func1(2); 
 ?>
 <br>
 <?php
-	function func2($num1, $num2, $num3) {
-		echo $num1 + $num2 + $num3;
-	}
-	func2(1, 2, 3); 
+    function func2($num1, $num2, $num3) {
+	echo $num1 + $num2 + $num3;
+    }
+    func2(1, 2, 3); 
 ?>
 <br>
 <?php
     function func3($param1, $param2, $param3) {
         echo $param1 + $param2 + $param3;
     }
-	$param1 = 1;
-	$param2 = 2;
-	$param3 = 3;
+    $param1 = 1;
+    $param2 = 2;
+    $param3 = 3;
     func3($param1, $param2, $param3); 
 
 ?>
