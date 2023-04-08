f8383d631d6e81fb9bfb8bd38cd6243258839a70
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-08 12:08:29 +0500

    Update 7_8.php

--- tasks/Shangin/7_Shangin/7_8.php
+++ tasks/Shangin/7_Shangin/7_8.php
@@ -1,12 +1,12 @@
 <?php
-	$show = true;
+    $show = true;
     if ($show === true){
         echo "<div><p1>text1</p2><br><p2>text2</p2><br><p3>text3</p3></div>";
     }
 ?>
 <br>
 <?php
-	$show = true;
+    $show = true;
     if ($show === true){
         echo "<div><p1>text+</p2><br><p2>text+</p2><br><p3>text+</p3></div>";
     } else {
@@ -15,7 +15,7 @@
 ?>
 <br>
 <?php
-	$show = true;
+    $show = true;
     if ($show == 2){
         echo "<div><p1>text1</p2><br><p2>text1</p2><br><p3>text1</p3></div>";
     } elseif ($show == 1){
