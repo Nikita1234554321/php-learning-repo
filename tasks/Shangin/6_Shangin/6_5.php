4a2d423a9f183790a84f6790ab1b2f7835223a1f
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-08 11:58:56 +0500

    Update 6_5.php

--- tasks/Shangin/6_Shangin/6_5.php
+++ tasks/Shangin/6_Shangin/6_5.php
@@ -1,13 +1,13 @@
 <?php
-	function isChetnei($arr) {
-		foreach ($arr as $elem) {
-			if ($elem % 2 === 0) {
-				return true;
-			} else {
+    function isChetnei($arr) {
+        foreach ($arr as $elem) {
+	    if ($elem % 2 === 0) {
+	        return true;
+	    } else {
                 return false;
             }
-		}
-	}
+        }
+    }
     $arr = [1, 3, 5, 7];
     $arr2 = [1, 4, 5];
     $arr3 = [2, 4, 6];
@@ -17,15 +17,15 @@
 ?>
 <br>
 <?php
-	function isNeChetnei($arr) {
-		foreach ($arr as $elem) {
-			if ($elem % 2 === 1) {
-				return true;
-			} else {
-                return false;
+    function isNeChetnei($arr) {
+        foreach ($arr as $elem) {
+	    if ($elem % 2 === 1) {
+	            return true;
+		} else {
+                    return false;
             }
-		}
-	}
+        }
+    }
     $arr = [3, 9, 5, 7];
     $arr2 = [3, 7, 5];
     $arr3 = [2, 4, 6];
@@ -35,15 +35,15 @@
 ?>
 <br>
 <?php
-	function same($arr) {
-		foreach ($arr as $elem) {
-			if ($elem == $elem) {
-				return true;
-			} else {
+    function same($arr) {
+        foreach ($arr as $elem) {
+	    if ($elem == $elem) {
+	        return true;
+	    } else {
                 return false;
             }
-		}
-	}
+        }
+    }
     $arr = [3, 3, 5, 7];
 
     var_dump (same($arr));
