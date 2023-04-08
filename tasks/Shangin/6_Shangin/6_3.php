6448de39354fac56b8b82f7d4a766e1b81be428a
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-08 11:55:23 +0500

    Update 6_3.php

--- tasks/Shangin/6_Shangin/6_3.php
+++ tasks/Shangin/6_Shangin/6_3.php
@@ -1,29 +1,29 @@
 <?php
-	function func($num) {
-		return $num * $num;
-	}
+    function func($num) {
+	return $num * $num;
+    }
 	
-	$result = func(3);
-	echo $result; 
+    $result = func(3);
+    echo $result; 
 ?>
 <br>
 <?php
-	function func1($num) {
-		return sqrt($num);
-	}
+    function func1($num) {
+	return sqrt($num);
+    }
 	
-	$result = func1(3) + func1(4);
-	echo $result; 
+    $result = func1(3) + func1(4);
+    echo $result; 
 ?>
 <br>
 <?php
-	function func2($num1, $num2) {
-		if ($num1 > 0 and $num2 > 0) {
-			return $num1 * $num2;
-		} else {
-			return $num1 - $num2;
-		}
+    function func2($num1, $num2) {
+	if ($num1 > 0 and $num2 > 0) {
+	    return $num1 * $num2;
+	} else {
+	    return $num1 - $num2;
 	}
+    }
 	
-	echo func2(3, 4);
+    echo func2(3, 4);
 ?>
