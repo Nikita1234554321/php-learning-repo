9ea4dd2c2f48bef5642a45797625a6fa6fc90588
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-08 11:37:20 +0500

    Update 6_1.php

--- tasks/Shangin/6_Shangin/6_1.php
+++ tasks/Shangin/6_Shangin/6_1.php
@@ -1,15 +1,15 @@
 <?php
-	function func() {
-		echo 'Никита';
-	}
-	func(); 
+    function func() {
+	echo 'Никита';
+    }
+    func(); 
 ?>
 <br>
 <?php
-	function funct() {
-	$arr = range(1, 100);
-    $summ = array_sum($arr);
-    echo $summ;
-	}
-	funct(); 
+    function funct() {
+    	$arr = range(1, 100);
+    	$summ = array_sum($arr);
+    	echo $summ;
+    }
+    funct(); 
 ?>
