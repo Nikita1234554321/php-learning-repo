bc62bddd4266ed0ad297b16586a7cfbd525d9249
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-15 13:41:24 +0500

    Update 10_10.php

--- tasks/Shangin/10_Shangin/10_10.php
+++ tasks/Shangin/10_Shangin/10_10.php
@@ -1,16 +1,16 @@
 <?php
-	ob_start();
-		include 'test.php'; 
-	$res = ob_get_clean(); 
+    ob_start();
+        include 'test.php'; 
+    $res = ob_get_clean(); 
 ?>
 <?php
-	function getFile($name) {
-		ob_start();
-			include $name; 
-		return ob_get_clean(); 
-	}
+    function getFile($name) {
+        ob_start();
+	    include $name; 
+        return ob_get_clean(); 
+    }
 ?>
 <?php
-	$res = getFile('test.php');
-	echo 'index' . $res;
+    $res = getFile('test.php');
+    echo 'index' . $res;
 ?>
