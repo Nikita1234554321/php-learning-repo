d9fc65273ffc0eaa0c8e10cab2b59ce99bb2202a
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-15 13:38:43 +0500

    Update 9_9.php

--- tasks/Shangin/9_Shangin/9_9.php
+++ tasks/Shangin/9_Shangin/9_9.php
@@ -1,9 +1,9 @@
 <?php
-	setcookie('test1', 'a', time() + 60 * 60 * 24 * 31);
+    setcookie('test1', 'a', time() + 60 * 60 * 24 * 31);
 
-	setcookie('test2', 'ab', time() + 60 * 60 * 24 * 365);
+    setcookie('test2', 'ab', time() + 60 * 60 * 24 * 365);
 
-	setcookie('test3', 'abc', time() + 60 * 60 * 24 * 3650);
+    setcookie('test3', 'abc', time() + 60 * 60 * 24 * 3650);
     
     echo $_COOKIE['test1'] . '<br>';
     echo $_COOKIE['test2'] . '<br>';
