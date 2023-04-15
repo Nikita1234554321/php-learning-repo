77fff962524855f5a47e19a3cc827b478d2fc2a3
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-15 13:26:36 +0500

    Update 9_5.php

--- tasks/Shangin/9_Shangin/9_5.php
+++ tasks/Shangin/9_Shangin/9_5.php
@@ -1,23 +1,23 @@
 <?php
-	session_start();
+    session_start();
 	
-	if (!empty($_GET)) {
-		$_SESSION['Фамилия'] = $_GET['Фамилия'];
-		$_SESSION['Имя'] = $_GET['Имя'];
-        $_SESSION['Возраст'] = $_GET['Возраст'];
-	}
+    if (!empty($_GET)) {
+    $_SESSION['Фамилия'] = $_GET['Фамилия'];
+    $_SESSION['Имя'] = $_GET['Имя'];
+    $_SESSION['Возраст'] = $_GET['Возраст'];
+    }
 ?>
 
 <?php
-	if (!empty($_SESSION)) {
-		echo $_SESSION['Фамилия'] . ' ' . $_SESSION['Имя'] . ' ' . $_SESSION['Возраст'];
-	}
+    if (!empty($_SESSION)) {
+        echo $_SESSION['Фамилия'] . ' ' . $_SESSION['Имя'] . ' ' . $_SESSION['Возраст'];
+    }
 ?>
 
 
 <form method="GET">
-	<input name="Фамилия" type='text' placeholder='Фамилия'>
-	<input name="Имя" type='text' placeholder='Имя'>
+    <input name="Фамилия" type='text' placeholder='Фамилия'>
+    <input name="Имя" type='text' placeholder='Имя'>
     <input name="Возраст" type='text' placeholder='Возраст'>
-	<input type="submit">
+    <input type="submit">
 </form>
