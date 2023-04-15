eb75c16c9f7cc4bd1a595910ddfb4723c9309e32
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-15 13:48:20 +0500

    Update 11_5.php

--- tasks/Shangin/11_Shangin/11_5.php
+++ tasks/Shangin/11_Shangin/11_5.php
@@ -1,11 +1,11 @@
 <?php
     $host = 'localhost'; // имя хоста
-	$user = 'root';      // имя пользователя
-	$pass = '';          // пароль
-	$name = 'mydb';      // имя базы данных
+    $user = 'root';      // имя пользователя
+    $pass = '';          // пароль
+    $name = 'mydb';      // имя базы данных
 	
-	$link = mysqli_connect($host, $user, $pass, $name);
-	mysqli_query($link, "SET NAMES 'utf8'");
+    $link = mysqli_connect($host, $user, $pass, $name);
+    mysqli_query($link, "SET NAMES 'utf8'");
 
     $query1 = "SELECT 'name', age, salary FROM users WHERE id  >= 1"
     $query2 = "SELECT 'name' FROM users WHERE id >= 1";
