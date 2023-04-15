7076f1c20ad34d0c1e92f3685fb4d8c8d133c286
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-15 13:45:07 +0500

    Update 10_5.php

--- tasks/Shangin/10_Shangin/10_5.php
+++ tasks/Shangin/10_Shangin/10_5.php
@@ -5,14 +5,14 @@
 <?php
     $arr = file ('s.txt',FILE_IGNORE_NEW_LINES);
     for ($i=0;$i<8;$i++) {
-    mkdir( $arr[$i] );
+        mkdir( $arr[$i] );
     }
 ?>
 <br>
 <?php
-	rmdir('test');
+    rmdir('test');
 ?>
 <br>
 <?php
-	rename('dir', 'test');
+    rename('dir', 'test');
 ?>
