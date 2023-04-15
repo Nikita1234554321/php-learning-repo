cbae1495a9126e9d358d6c7efa38a5e577d335d3
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-15 13:48:33 +0500

    Update 11_6.php

--- tasks/Shangin/11_Shangin/11_6.php
+++ tasks/Shangin/11_Shangin/11_6.php
@@ -1,12 +1,12 @@
 <?php
     $host = 'localhost'; // имя хоста
-	$user = 'root';      // имя пользователя
-	$pass = '';          // пароль
-	$name = 'mydb';      // имя базы данных
+    $user = 'root';      // имя пользователя
+    $pass = '';          // пароль
+    $name = 'mydb';      // имя базы данных
 	
-	$link = mysqli_connect($host, $user, $pass, $name);
-	mysqli_query($link, "SET NAMES 'utf8'");
+    $link = mysqli_connect($host, $user, $pass, $name);
+    mysqli_query($link, "SET NAMES 'utf8'");
 
     $query = "INSERT INTO users ('name', age, salary) VALUES ('user7', 27, 300)";
-	mysqli_query($link, $query) or die(mysqli_error($link));
+    mysqli_query($link, $query) or die(mysqli_error($link));
 ?>
