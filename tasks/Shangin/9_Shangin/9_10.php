0a72ec8b169ea838e55aa2cbd17d7a5f4402bfd6
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-15 13:25:05 +0500

    Update 9_10.php

--- tasks/Shangin/9_Shangin/9_10.php
+++ tasks/Shangin/9_Shangin/9_10.php
@@ -1,13 +1,13 @@
 <?php
-	setcookie('test', '', time());
-	var_dump($_COOKIE['test']); 
+    setcookie('test', '', time());
+    var_dump($_COOKIE['test']); 
 ?>
 <br>
 <?php
-	if (isset($_COOKIE['test'])) {
-		setcookie('test', '', time());
-		unset($_COOKIE['test']);
-	}
+    if (isset($_COOKIE['test'])) {
+        setcookie('test', '', time());
+        unset($_COOKIE['test']);
+    }
 	
-	var_dump($_COOKIE['test']); 
+    var_dump($_COOKIE['test']); 
 ?>
