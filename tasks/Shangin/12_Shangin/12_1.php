ddd9772e0eabeb4ec4dd1c648087d067dcf09d10
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-21 11:47:42 +0500

    Update 12_1.php

--- tasks/Shangin/12_Shangin/12_1.php
+++ tasks/Shangin/12_Shangin/12_1.php
@@ -1,40 +1,20 @@
-<!-- SELECT products.id, category.type_category
-FROM 
-	products
-LEFT JOIN category ON products.id=category.type_category -->
+<!-- SELECT products.id, category.type_category FROM products LEFT JOIN category ON products.id=category.type_category -->
 
 
-<!-- SELECT 
-	products.id,
-    podcategory.podcategory_type,
-	category.type_category
-FROM 
-	products
-LEFT JOIN category ON products.id=category.type_category
-LEFT JOIN podcategory ON podcategory.podcategory_type=category.type_category -->
+<!-- SELECT products.id, podcategory.podcategory_type, category.type_category FROM products 
+LEFT JOIN category ON products.id=category.type_category LEFT JOIN podcategory ON podcategory.podcategory_type=category.type_category -->
 
 
-<!-- SELECT
-	products.id, category.type_category
-FROM
-	products
+<!-- SELECT products.id, category.type_category FROM products
 LEFT JOIN products_type ON products_type.products.id=uproducts.id
 LEFT JOIN category ON products_type.id=category.type_category -->
 
 
-<!-- SELECT
-	products.id as products_id, product.name as product_name
-FROM
-	products
+<!-- SELECT products.id as products_id, product.name as product_name FROM products
 LEFT JOIN products as product ON product.id=products.products_id -->
 
 
-<!-- SELECT
-	users.name as user_name,
-	fathers.name as father_name,
-    mother.name as mother_name
-FROM
-	users
+<!-- SELECT users.name as user_name, fathers.name as father_name, mother.name as mother_name FROM users
 LEFT JOIN users as fathers ON fathers.id=users.father_id
 LEFT JOIN users as mother ON mother.id=users.mother_id -->
 
