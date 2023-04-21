c87740e0f60fef1447d177b48828a999eba51242
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-21 11:52:59 +0500

    Update form.php

--- tasks/Shangin/14_Shangin/form.php
+++ tasks/Shangin/14_Shangin/form.php
@@ -1,25 +1,22 @@
 <form method="POST">
-	<input name="test1">
-	<input name="test2">
-	<input type="submit">
+    <input name="test1">
+    <input name="test2">
+    <input type="submit">
 </form>
 
 <?php
-	session_start();
+    session_start();
 	
-	if (!empty($_POST)) {
-		if (валидация формы) {
-
-			
-			$_SESSION['flash'] = 'форма успешно сохранена';
-			header('Location: form.php');
-		} else {
-			$_SESSION['flash'] = 'форма не прошла валидацию';
-		}
-	}
-	
-	if (isset($_SESSION['flash'])) {
-		echo $_SESSION['flash'];
-		unset($_SESSION['flash']);
-	}
+    if (!empty($_POST)) {
+        if (валидация формы) {	
+	    $_SESSION['flash'] = 'форма успешно сохранена';
+	    header('Location: form.php');
+        } else {
+	    $_SESSION['flash'] = 'форма не прошла валидацию';
+        }
+    }
+    if (isset($_SESSION['flash'])) {
+        echo $_SESSION['flash'];
+        unset($_SESSION['flash']);
+    }
 ?>
