777d9c45f77dd20fc23559d711d01fd6d19aaa99
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-21 11:51:37 +0500

    Update 14_2.php

--- tasks/Shangin/14_Shangin/14_2.php
+++ tasks/Shangin/14_Shangin/14_2.php
@@ -1,11 +1,11 @@
 <?php
-	header('Location: index.php?success=1');
+    header('Location: index.php?success=1');
 ?>  
 <br>
 <?php
-	if (!isset($_GET['num'])) {
-		header('Location: ?num=1');
-	}
+    if (!isset($_GET['num'])) {
+        header('Location: ?num=1');
+    }
 	
-	echo $_GET['arg']; 
+    echo $_GET['arg']; 
 ?>
