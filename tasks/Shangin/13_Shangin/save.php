f474f46d28b4b22bae724818391bbe2a4c9f20aa
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-04-21 11:50:22 +0500

    Update save.php

--- tasks/Shangin/13_Shangin/save.php
+++ tasks/Shangin/13_Shangin/save.php
@@ -8,9 +8,9 @@
     mysqli_query($link, "SET NAMES 'utf8'");
 
     $id = $_GET['id'];
-	$name = $_POST['name'];
-	$age = $_POST['age'];
-	$salary = $_POST['salary'];
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $salary = $_POST['salary'];
 
     $query = "UPDATE users SET name='$name', age='$age', salary='$salary' WHERE id=$id";
     mysqli_query($link, $query) or die(mysqli_error($link));
